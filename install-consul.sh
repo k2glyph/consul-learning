f1061f3bb953c7f979dd3ac706da05662584eb5b
Dinesh katwal <dinesh.katwal@logicabeans.com>
2018-10-10 16:53:32 +0545

    added simple color for terminal

--- install-consul.sh
+++ install-consul.sh
@@ -2,6 +2,17 @@
 sudo apt-get update
 sudo apt-get install -y unzip
 
+
+#Theming terminal
+cat >> ~/.bashrc <<"END"
+# Coloring of hostname in prompt to keep track of what's what in demos, blue provides a little emphasis but not too much like red
+NORMAL="\[\e[0m\]"
+BOLD="\[\e[1m\]"
+DARKGRAY="\[\e[90m\]"
+BLUE="\[\e[34m\]"
+PS1="$DARKGRAY\u@$BOLD$BLUE\h$DARKGRAY:\w\$ $NORMAL"
+END
+
 CONSUL_VERSION=0.6.4
 
 curl https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_amd64.zip -o consul.zip
